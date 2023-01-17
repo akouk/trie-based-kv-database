860882b7353b097affc86b9fd48903ba2bd594b5
akouk <77749065+akouk@users.noreply.github.com>
2023-01-17 23:45:11 +0200

    Update KVServer.py

--- KVServer.py
+++ KVServer.py
@@ -1,30 +1,33 @@
-import BaseServer as BaseServer
 import re
 import numbers
 import math
+import typing as tp
+
 
 class TrieNode:
     def __init__(self) -> None:
-        self.children = {} # a dictionary mapping characters to child nodes
-        self.value = None # the value stored at this node (if any)
-        self.is_leaf = False # a boolean attribute that indicates whether the currnet node is a leaf node in the trie or not
-        self.is_deleted = False # a flag indicating whether the node has been deleted
+        self.children = {}  # a dictionary mapping characters to child nodes
+        self.value = None  # the value stored at this node (if any)
+        # a boolean attribute that indicates whether the current node is a leaf node in the trie or not
+        self.is_leaf = False
+        self.is_deleted = False  # a flag indicating whether the node has been deleted
 
     def remove_if_unused(self) -> None:
-        if self.is_deleted and not self.children: # remove the node if it has no children and is marked as deleted
+        if self.is_deleted and not self.children:  # remove the node if it has no children and is marked as deleted
             del self
+
 
 class Trie:
     def __init__(self) -> None:
-        self.root = TrieNode() # create the root node of the trie
-        self.variables = {} # dictionary to store variable names and their values
-        self.operators = {'+':1, '-':1, '*':2, '/':2, '^':3}
-        self.functions = {'sin':1, 'cos':1, 'tan':1, 'log':1}
+        self.root = TrieNode()  # create the root node of the trie
+        self.variables = {}  # dictionary to store variable names and their values
+        self.operators = {'+': 1, '-': 1, '*': 2, '/': 2, '^': 3}
+        self.functions = {'sin': 1, 'cos': 1, 'tan': 1, 'log': 1}
         self.left_associative_operators = ['+', '-', '*', '/']
         self.computed_value = None
 
     def put(self, key: str, value: str) -> None:
-        current_root = self.root # start at the root of the trie
+        current_root = self.root  # start at the root of the trie
         # follow the path of the key in the trie, creating new nodes as needed
         for char in key:
             # print(f'char: {char}')
@@ -32,11 +35,10 @@ class Trie:
                 # print('chzr not in current_root.children')
                 current_root.children[char] = TrieNode()
             current_root = current_root.children[char]
-        
-        current_root.is_leaf = True
-        current_root.value = value #store the value at the final node
 
-   
+        current_root.is_leaf = True
+        current_root.value = value  # store the value at the final node
+
     def get(self, key: str) -> str:
         current_root = self.root
         # follow the path of the key in the trie, creating new nodes as needed
@@ -47,20 +49,23 @@ class Trie:
                 # the key does not exist in the trie
                 return None
             current_root = current_root.children[char]
-            # print(f'current_root = {current_root.childern}')
-        
+            # print(f'current_root = {current_root.children}')
+
+        if current_root == "null":
+            return "null"
+
         if current_root.is_leaf:
             if not current_root.is_deleted:
                 return current_root.value
             else:
                 return f'Key: {key} deleted successfully!'
         else:
-            return None
+            return "null"
 
     def delete(self, key: str) -> str:
         current_root = self.root
         # follow the path of the key in the trie, creating new nodes as needed
-        path = []
+        path: tp.List[TrieNode] = []
         for char in key:
             if char not in current_root.children:
                 # the key is not in the trie
@@ -82,7 +87,7 @@ class Trie:
         high_level_key = keys[0]
         value_of_high_level_key = self.get(high_level_key)
 
-        def search_dictionary( dic, keys):
+        def search_dictionary(dic, keys):
             for key in keys[1:]:
                 print(f'key: {key}')
                 if key in dic:
@@ -92,7 +97,7 @@ class Trie:
 
             if isinstance(dic, dict):
                 search_dictionary(dic, keys)
-            
+
             return dic
 
         if value_of_high_level_key == None:
@@ -100,13 +105,13 @@ class Trie:
             # keypath_value == None
         else:
             if isinstance(value_of_high_level_key, dict):
-                keypath_value = search_dictionary(value_of_high_level_key, keys)
+                keypath_value = search_dictionary(
+                    value_of_high_level_key, keys)
                 # print(f'keypath_value: {keypath_value}')
                 # print(f'{keypath} -> ' + print_key_value(keypath_value))
-            
+
         return keypath_value
 
-        
     @staticmethod
     def is_operator(character, operator_precedence):
         return character in operator_precedence
@@ -127,7 +132,7 @@ class Trie:
     @staticmethod
     def is_left_associative(character, left_associative_operators):
         return character in left_associative_operators
-        
+
     @staticmethod
     def perform_operation(operand1, operand2, operator):
         if operator == '+':
@@ -178,7 +183,8 @@ class Trie:
                     trie.is_operator(operator_stack[-1], operators) and
                     (
                         trie.precedence(operator_stack[-1], operators) > trie.precedence(character, operators) or
-                        (trie.precedence(operator_stack[-1], operators) == trie.precedence(character, operators) and trie.is_left_associative(character, left_associative_operators))
+                        (trie.precedence(operator_stack[-1], operators) == trie.precedence(
+                            character, operators) and trie.is_left_associative(character, left_associative_operators))
                     )
                 ):
                     output_queue.append(operator_stack.pop())
@@ -196,7 +202,7 @@ class Trie:
         while operator_stack:
             output_queue.append(operator_stack.pop())
 
-        return output_queue    
+        return output_queue
 
     @staticmethod
     def compute_expression(
@@ -215,7 +221,8 @@ class Trie:
                 # Pop the last two elements from the stack as operands
                 operand2 = stack.pop()
                 operand1 = stack.pop()
-                operation_result = trie.perform_operation(operand1, operand2, operator)
+                operation_result = trie.perform_operation(
+                    operand1, operand2, operator)
                 # Perform the operation and push the result back to the stack
                 stack.append(operation_result)
 
@@ -228,7 +235,7 @@ class Trie:
 
         # The final result is the last element in the stack
         return stack.pop()
-    
+
     def compute(self, advanced_formula):
         # Extract the formula and keypath
         computation, variable_keypath = advanced_formula.split('WHERE')
@@ -236,29 +243,31 @@ class Trie:
         computation_formula = re.search(r'COMPUTE (.*)', computation)
         if not computation_formula:
             return 'Invalid computation formula'
-        
+
         variables = self.variables
         if 'AND' in variable_keypath:
             variable_values = variable_keypath.split('AND')
             for variable_value in variable_values:
 
-                match_value_formula = re.search(r'(\w+) = QUERY (.*)', variable_value)          
+                match_value_formula = re.search(
+                    r'(\w+) = QUERY (.*)', variable_value)
                 if not match_value_formula:
                     return 'Invalid value formula'
 
                 variable_name = match_value_formula.group(1).strip()
                 variable_value_keypath = match_value_formula.group(2).strip()
                 variable_value = self.query(variable_value_keypath)
-                
+
                 if isinstance(variable_value, numbers.Number):
                     variables[variable_name] = variable_value
                 else:
                     return 'Keypath s value is not a number'
         else:
-            match_value_formula = re.search(r'(\w+) = QUERY (.*)', variable_keypath)          
+            match_value_formula = re.search(
+                r'(\w+) = QUERY (.*)', variable_keypath)
             if not match_value_formula:
                 return 'Invalid value formula'
-            
+
             variable_name = match_value_formula.group(1).strip()
             variable_value_keypath = match_value_formula.group(2).strip()
             variable_value = self.query(variable_value_keypath)
@@ -266,7 +275,7 @@ class Trie:
             if isinstance(variable_value, numbers.Number):
                 variables[variable_name] = variable_value
             else:
-                return 'Keypath s is not a number'  
+                return 'Keypath s is not a number'
 
         operators = self.operators
         functions = self.functions
@@ -275,8 +284,9 @@ class Trie:
         # Replace the variables with their values in the formula
         for variable, value in variables.items():
             computation = computation.replace(variable, str(value))
-        
-        operators_and_variables = re.findall('\d+|[+-/*^()]|sin|cos|tan|log10', computation)
+
+        operators_and_variables = re.findall(
+            '\d+|[+-/*^()]|sin|cos|tan|log10', computation)
         if not operators_and_variables:
             return 'Invalid operators'
 
@@ -294,25 +304,26 @@ class Trie:
         return(computed_result)
 
 
-class KVServer(BaseServer.BaseServer):
-    def __init__(self, port, ip_address):
-        super().__init__(port=port, host=ip_address)
-        self.ip_address = ip_address
-        self.port = port
+class KVServer:
+    def __init__(self):
         self.root = Trie()
 
     def put_request(self, key, value):
-        self.root.put(key, value) # insert the key-value pair into the trie
+        self.root.put(key, value)  # insert the key-value pair into the trie
 
     def get_request(self, key):
-        return self.root.get(key) # retrieve the value associated with the key from the trie
+        # retrieve the value associated with the key from the trie
+        return self.root.get(key)
 
     def delete_request(self, key):
-        self.root.delete(key) # delete the key and its associated value from the trie
+        # delete the key and its associated value from the trie
+        self.root.delete(key)
 
     def query_request(self, keypath):
-        return self.root.query(keypath) # retrieve the value or subkey associated with the keypath from the trie
-            
+        # retrieve the value or subkey associated with the keypath from the trie
+        return self.root.query(keypath)
+
     def compute_request(self, formula):
         # Parse the formula and retrieve any required values from the key-value store
-        return self.root.compute(formula) # compute formula using values from the trie
+        # compute formula using values from the trie
+        return self.root.compute(formula)
