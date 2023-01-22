f5fb7ea11ea37529e5fba917483cbf8ac86eef1c
akouk <77749065+akouk@users.noreply.github.com>
2023-01-22 11:03:11 +0200

    Update MathExpressionEvaluator.py

--- MathExpressionEvaluator.py
+++ MathExpressionEvaluator.py
@@ -1,38 +1,67 @@
-import math
+import math 
+from decimal import Decimal
 import typing as tp
 
 class MathExpressionEvaluator:
-    
+
+    # A function to check if the given character is an operator
     @staticmethod
     def is_operator(character: str, operator_precedence: dict) -> bool:
-        # Check if the character is in the operator_precedence dict
+        """
+        Check if the given character is an operator.
+        The operator_precedence is used to check the operator precedence
+        """
         return character in operator_precedence
 
+    # A function to check the precedence of an operator
     @staticmethod
     def precedence(character: str, operator_precedence: dict) -> int:
-        # Return the precedence of the character from the operator_precedence dict
+        """
+        Get the precedence of the operator
+        """
         return operator_precedence[character]
 
+    # A function to check if the given character is a function
     @staticmethod
     def is_function(character: str, function_precedence: dict) -> bool:
-        # Check if the character is in the function_precedence dict
+        """
+        Check if the given character is a function
+        The function_precedence is used to check the function precedence
+        """
         return character in function_precedence
 
     @staticmethod
-    def is_number(character: str) -> bool:
-        # Check if the character is numeric
-        if character.isnumeric():
+    def is_float(character):
+        try:
+            float(character)
             return True
-        return False
-
+        except ValueError:
+            return False
+    
+    @staticmethod
+    def is_int(character: str) -> bool:
+        try:
+            int(character)
+            return True
+        except ValueError:
+            return False
+       
+    # A function to check if the given operator is left-associative
     @staticmethod
     def is_left_associative(character: str, left_associative_operators: list) -> bool:
-        # Check if the character is in the left_associative_operators list
+        """
+        Check if the given operator is left associative
+        """
         return character in left_associative_operators
-
+        
+    # A function to perform a mathematical operation between two operands using the given operator
     @staticmethod
-    def perform_operation(operand1: float, operand2: float, operator: str) -> float:
-        # Perform the operation based on the operator and return the result
+    def perform_operation(operand1, operand2, operator):
+        """
+        Perform the operation based on the operator
+        """
+        operand1 = Decimal(operand1)
+        operand2 = Decimal(operand2)
         if operator == '+':
             return operand1 + operand2
         elif operator == '-':
@@ -43,12 +72,12 @@ class MathExpressionEvaluator:
             return operand1 / operand2
         elif operator == '^':
             return operand1 ** operand2
-        else:
-            return 'Invalid operand!'
+        
+        return 'Invalid operand!'
 
     @staticmethod
-    def perform_function(function: str, operand: float) -> float:
-        # Perform the function based on the function name and return the result
+    def perform_function(function, operand):
+        operand = Decimal(operand)
         if function == 'sin':
             return math.sin(operand)
         elif function == 'cos':
@@ -57,9 +86,8 @@ class MathExpressionEvaluator:
             return math.tan(operand)
         elif function == 'log':
             return math.log10(operand)
-        else:
-            return 'Invalid function!'
-
+    
+        return 'Invalid function!'
 
     def evaluate_expression(
         self,
@@ -67,19 +95,22 @@ class MathExpressionEvaluator:
         operators: dict,
         functions: dict,
         left_associative_operators: tp.List[str]
-    ) -> tp.List[tp.Union[str, int]]:
-
+        ) -> tp.List[tp.Union[str, int]]:
         # Initialize two empty lists, one for output and one for operators
         output_queue = []
         operator_stack = []
-
         # Iterate through the characters
         while characters:
             character = characters.pop(0)
-            # Check if the current character is a number
-            if self.is_number(character):
+            # Check if the current character is an int 
+            if self.is_int(character):
+                # Append the character to the queue as int
                 output_queue.append(character)
-            # Check if the current character is a function
+            # Check if the current character is a float 
+            elif self.is_float(character):
+                # Append the number to the queue as float
+                output_queue.append(character)
+            # Check if the current character is a function and add it to stack
             elif self.is_function(character, functions):
                 operator_stack.append(character)
             # Check if the current character is an operator
@@ -91,7 +122,8 @@ class MathExpressionEvaluator:
                     self.is_operator(operator_stack[-1], operators) and
                     (
                         self.precedence(operator_stack[-1], operators) > self.precedence(character, operators) or
-                        (self.precedence(operator_stack[-1], operators) == self.precedence(character, operators) and self.is_left_associative(character, left_associative_operators))
+                        (self.precedence(operator_stack[-1], operators) == self.precedence(
+                            character, operators) and self.is_left_associative(character, left_associative_operators))
                     )
                 ):
                     # Pop elements from the operator stack and append to output queue
@@ -99,16 +131,16 @@ class MathExpressionEvaluator:
                 # Append the current operator to the operator stack
                 operator_stack.append(character)
             # Check if the current character is "("
-            elif character == "(":
+            elif character == '(':
                 operator_stack.append(character)
             # Check if the current character is ")"
-            elif character == ")":
+            elif character == ')':
                 # While the operator stack is not empty and the last element is not "("
-                while operator_stack and operator_stack[-1] != "(":
+                while operator_stack and operator_stack[-1] != '(':
                     # Pop elements from the operator stack and append to output queue
                     output_queue.append(operator_stack.pop())
                 # If the last element of operator stack is "(", pop it
-                if operator_stack and operator_stack[-1] == "(":
+                if operator_stack and operator_stack[-1] == '(':
                     operator_stack.pop()
                 # Check if the last element of operator stack is a function, 
                 # if so pop it and append to the output queue
@@ -120,7 +152,7 @@ class MathExpressionEvaluator:
             output_queue.append(operator_stack.pop())
 
         # Return the output queue
-        return output_queue  
+        return output_queue
 
     def compute_expression(
             self,
@@ -129,31 +161,35 @@ class MathExpressionEvaluator:
             functions: dict
         ) -> tp.Union[float, str]:
 
-            # Use a stack to handle operator precedence
-            stack = []
-            # Print the evaluated expression for debugging
-            print(f"result: {evaluated_expression}")
-            # Iterate through the evaluated expression
-            for character in evaluated_expression:
-                # Check if the current character is a number
-                if self.is_number(character):
-                    # Append the number to the stack as float
-                    stack.append(float(character))
-                # Check if the current character is an operator
-                elif self.is_operator(character, operators):
-                    operator = character
-                    # Pop the last two elements from the stack as operands
-                    operand2 = stack.pop()
-                    operand1 = stack.pop()
-                    # Perform the operation and push the result back to the stack
-                    operation_result = self.perform_operation(operand1, operand2, operator)
-                    stack.append(operation_result)
-                # Check if the current character is a function
-                elif self.is_function(character, functions):
-                    function = character
-                    operand = stack.pop()
-                    # Perform the function and push the result back to the stack
-                    function_result = self.perform_function(function, operand)
-                    stack.append(function_result)
-            # The final result is the last element in the stack
-            return stack.pop()
+        # Use a stack to handle operator precedence
+        stack = []
+        # Iterate through the evaluated expression
+        for character in evaluated_expression:
+            # Check if the current character is an int
+            if self.is_int(character):
+                # Append the number to the stack as int
+                stack.append(int(character))
+            # Check if the current character is a float
+            elif self.is_float(character):
+                # Append the number to the stack as float
+                stack.append(float(character))
+            # Check if the current character is an operator
+            elif self.is_operator(character, operators):
+                operator = character
+                # Pop the last two elements from the stack as operands
+                operand2 = stack.pop()
+                operand1 = stack.pop()
+                operation_result = self.perform_operation(
+                    operand1, operand2, operator)
+                # Perform the operation and push the result back to the stack
+                stack.append(operation_result)
+
+            elif self.is_function(character, functions):
+                function = character
+                operand = stack.pop()
+                # Perform the function and push the result back to the stack
+                function_result = self.perform_function(function, operand)
+                stack.append(function_result)
+
+        # The final result is the last element in the stack
+        return stack.pop()
